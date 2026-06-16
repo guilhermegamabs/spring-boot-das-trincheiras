38945564b37d27aace1e244266930ad2c1c2c2b3
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2026-06-15 21:28:18 -0300

    PathVariable

--- anime-service/src/main/java/academy/devdojo/controllers/HeroController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/HeroController.java
@@ -1,9 +1,6 @@
 package academy.devdojo.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +22,13 @@ public class HeroController {
     @GetMapping("filterList")
     public List<String> listAllHeroesParamList(@RequestParam List<String> names) {
         return HEROES.stream().filter(names::contains).toList();
+    }
+
+    @GetMapping("{name}")
+    public String findByName(@PathVariable String name) {
+        return HEROES
+                .stream()
+                .filter(heroes -> heroes.equalsIgnoreCase(name))
+                .findFirst().orElse("");
     }
 }
